ff20722178e781c3e6b89a9aeca5e17c4faac0f5
ievensen <idar.evensen@gmail.com>
2025-07-14 13:12:14 +0200

    change - new hostname for localhost

--- system/hardware/network.nix
+++ system/hardware/network.nix
@@ -7,7 +7,7 @@
     useDHCP = lib.mkDefault true;
     firewall.enable = false;
     extraHosts = ''
-      127.0.0.1 tuathaan
+      127.0.0.1 codebook
       104.199.65.124 ap-gew4.spotify.com
     '';
   };
